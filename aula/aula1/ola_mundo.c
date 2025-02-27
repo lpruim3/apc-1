fe6dc187fdc8975fc53abe322f50ee0e28d0ca7a
Lucas Pessoa <Lucaspranieri@gmail.com>
2025-02-26 21:41:53 -0300

    codigos da aula 01

---
+++ aula/aula1/ola_mundo.c
@@ -0,0 +1,10 @@
+ //função pra comentar"//" 
+ //função para comentar só que em linhas "/**/"
+#include <stdio.h> // inclusao de bibliotecas
+
+// funcao principal que inicia o programa
+int main() {
+    printf("Ola pessoal!\n");
+
+    return 0;
+}
