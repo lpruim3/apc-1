7d8f59d48881eacbc0a1dbd3b46efc6a428635a8
Lucas Pessoa <Lucaspranieri@gmail.com>
2025-03-12 21:49:40 -0300

    codigos da aula 2

---
+++ aula/aula2/imprimir.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+    printf("Boa noite\n");
+    printf("\tEstamos aprendendo C!");
+    return 0;
+}
+
---
+++ aula/aula2/imprimir_caracter.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(){
+    printf("%c\n", 'A');
+    printf("Ola %s\n", "Lucas");
+    return 0;
+}
