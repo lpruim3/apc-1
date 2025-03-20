336011e8448749f008fbe1b75de382b8fc6063b5
Lucas Pessoa <Lucaspranieri@gmail.com>
2025-03-19 21:51:14 -0300

    Aula pratica 2

---
+++ praticas/pratica02/imprima_inteiros.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main(){
+    printf("%i\n", 1234);
+    printf("%i\n", 56789);
+    printf("%05i\n", 1);
+    printf("(%i, %i)\n", -1, 1);
+    printf("%i/%02i/%4i\n", 19, 3, 2024);
+    return 0;
+}
